ae01ecc19aab6313e5e67a7cb889f407fcdd43c9
Níz Peña <nizadiel5@gmail.com>
2025-08-10 08:30:54 -0700

    RQ-02: Sistema de puntaje añadido

---
+++ scripts/utils/global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Almacena datos pendientes de cargar después de cambiar de escena
+var pending_load_data = null
+
+# Almacena la última copia de los datos guardados en memoria
+var last_save_data = null
